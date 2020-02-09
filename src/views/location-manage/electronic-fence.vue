69876fa7494bcc5d913164db1866e4e010ff2b6e
flyreve <1361901282@qq.com>
2020-02-09 10:49:24 +0800

    Add 电子围栏 & 任务区域

--- src/views/location-manage/electronic-fence.vue
+++ src/views/location-manage/electronic-fence.vue
@@ -5,6 +5,19 @@
       <div slot="header">
         <span class="fz-16">电子围栏</span>
       </div>
+      <el-input
+              v-model="searchKey"
+              placeholder="搜索"
+              size="mini"
+      >
+        <i slot="prefix" class="el-input__icon el-icon-search" />
+      </el-input>
+      <div class="remarks fz-10 mt-5">已创建的电子围栏</div>
+      <div class="enclosure-list mt-5">
+        <div class="enclosure-item" v-for="(i, j) in 9" :key="j + 'e'">
+          xxxxx（电子围栏名称）
+        </div>
+      </div>
     </el-card>
   </div>
 </template>
@@ -13,7 +26,9 @@
   export default {
     name: 'electronic-fence',
     data() {
-      return {}
+      return {
+        searchKey: ''
+      }
     },
     mounted() {
     },
@@ -21,4 +36,21 @@
   }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+  .electronic-fence {
+    .enclosure-list {
+      height: calc(100% - 52px);
+      overflow-y: scroll;
+      .enclosure-item {
+        cursor: pointer;
+        display: flex;
+        justify-content: space-between;
+        padding: 5px 5px 5px 3px;
+        border-bottom: 1px dashed #D0CECE;
+        &:hover {
+          background: #F0F0F0;
+        }
+      }
+    }
+  }
+</style>
--- src/views/location-manage/task-area.vue
+++ src/views/location-manage/task-area.vue
@@ -5,6 +5,19 @@
       <div slot="header">
         <span class="fz-16">任务区域</span>
       </div>
+      <el-input
+              v-model="searchKey"
+              placeholder="搜索"
+              size="mini"
+      >
+        <i slot="prefix" class="el-input__icon el-icon-search" />
+      </el-input>
+      <div class="remarks fz-10 mt-5">已创建的任务区域</div>
+      <div class="area-list mt-5">
+        <div class="area-item" v-for="(i, j) in 9" :key="j + 'e'">
+          xxxxx（任务区域名称）
+        </div>
+      </div>
     </el-card>
   </div>
 </template>
@@ -21,4 +34,21 @@
   }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+  .task-area {
+    .area-list {
+      height: calc(100% - 52px);
+      overflow-y: scroll;
+      .area-item {
+        cursor: pointer;
+        display: flex;
+        justify-content: space-between;
+        padding: 5px 5px 5px 3px;
+        border-bottom: 1px dashed #D0CECE;
+        &:hover {
+          background: #F0F0F0;
+        }
+      }
+    }
+  }
+</style>
